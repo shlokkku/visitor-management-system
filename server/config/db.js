7453ae881d7254a76c8967070e91854654f73fa9
Maryam1909 <maryam.bahlooli66@gmail.com>
2025-05-03 04:07:16 +0530

    final

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,12 @@
-//server/config/db.js
 const mysql = require('mysql2/promise');
+require('dotenv').config(); // Ensure dotenv is loaded to access environment variables
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',  // replace with your actual MySQL username
-  password: 'Bangtanb-123',  // replace with your actual MySQL password
-  database: 'visitormanagement',  // using your database name
+  host: process.env.DB_HOST,            // Updated to use DB_HOST
+  user: process.env.DB_USER,            // Updated to use DB_USER
+  password: process.env.DB_PASSWORD,    // Updated to use DB_PASSWORD
+  database: process.env.DB_NAME,        // Updated to use DB_NAME
+  port: process.env.DB_PORT,            // Updated to use DB_PORT
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
